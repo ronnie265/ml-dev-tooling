f2d46f17a3fe9102952aa39e5be22f6ba303c92d
sandor.nagy <sandor.nagy@ocado.com>
2023-08-13 17:13:16 +0100

    Using poetry instead of pip.

--- init_env.sh
+++ init_env.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -e
 
 export PYTHONPATH=$PYTHONPATH:$PWD
-python3 -m venv venv  # recommend using Python 3.10
-source venv/bin/activate  # on Windows: venv\Scripts\activate
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install -r requirements.txt
-pre-commit install
-pre-commit autoupdate
+pip install --quiet 'poetry==1.2.0'
+poetry run pip install --quiet --upgrade pip
+poetry install
