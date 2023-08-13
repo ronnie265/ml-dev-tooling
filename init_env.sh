3a12d061415c6729854e053a3b9a05da4ccbaf02
sandor.nagy <sandor.nagy@ocado.com>
2023-08-13 17:18:09 +0100

    Higher version of poetry.

--- init_env.sh
+++ init_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 export PYTHONPATH=$PYTHONPATH:$PWD
-pip install --quiet 'poetry==1.2.0'
+pip install --quiet 'poetry==1.4.1'
 poetry run pip install --quiet --upgrade pip
 poetry install
